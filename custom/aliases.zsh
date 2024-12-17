b8ddcb7db4cfb8431c81ec26bac3109a7706ecfe
Freddy Marten <martenfreddy@gmail.com>
2024-12-17 18:56:32 +0000

    Initial Commit

---
+++ custom/aliases.zsh
@@ -0,0 +1,12 @@
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+#
+# NOTE: My aliases:
+#
+# Copy the PWD to the Clipboard
+alias cpwd="pwd | tr -d '\n' | pbcopy && echo 'pwd copied to clipboard'"
+# Start ipython 
+alias ip="ipython"
+# Start neovim
+alias nv="nvim"
